ca3192063ee116af98ab3dbe67af92a1b63c09fd
Doug Johnston <doug.johnston@gmail.com>
2010-01-18 22:12:54 -0800

    Add my (not so secret) Secret Keys for accessing Evernote API

---
+++ EvernoteKeys.cs
@@ -0,0 +1,10 @@
+
+using System;
+
+namespace Tomboy.Sync
+{
+ 	public static class EvernoteKeys {
+		public const string ConsumerKey = "dvjohnston";
+        public const string ConsumerSecret = "735e66c24ca3b232"; 	
+	}
+}
